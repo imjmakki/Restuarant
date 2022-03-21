29d22403719fb92ec6112895926dc012ecb85637
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-21 09:54:35 +0300

    Staring back-end

---
+++ src/main/java/app/restaurant/restauring/RestauRingApplication.java
@@ -0,0 +1,13 @@
+package app.restaurant.restauring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauRingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauRingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/restaurant/restauring/RestauRingApplicationTests.java
@@ -0,0 +1,13 @@
+package app.restaurant.restauring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauRingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
